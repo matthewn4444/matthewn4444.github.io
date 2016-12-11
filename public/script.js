7e0cf1442aea1c35d9faf3d656e1dff4bcf7acbf
Matthew Ng <matthewn4444@gmail.com>
2016-12-10 18:16:56 -0800

    Fixes the visibility of the buffer animation state

--- public/script.js
+++ public/script.js
@@ -1,27 +1,25 @@
-var BUFFER_END_TIMEOUT = 50;
-
 function showBufferAnimation() {
-    $("#progress").text("0%");
-    $("#buffer").fadeIn();
-    window.lastBufferPercent = 0;
+    if (!$("#buffer").is(":visible")) {
+        $("#progress").text("0%");
+        $("#buffer").fadeIn();
+        window.lastBufferPercent = 0;
+    }
     window.isBuffering = true;
 }
 
 function hideBufferAnimation() {
+try {
     window.isBuffering = false;
     if ($("#buffer").is(":visible")) {
         $("#buffer").fadeOut(200, function() {
             $(document.body).removeClass("preload");
         });
     }
-    if (window.bufferTimer) {
-        clearInterval(window.bufferTimer);
-        window.bufferTimer = null;
-    }
     if (window.seekPokeTimer) {
         clearInterval(window.seekPokeTimer);
         window.seekPokeTimer = null;
     }
+    } catch (e) {console.log(e)}
 }
 
 function newPlayer() {
@@ -43,7 +41,6 @@ window.onload = function() {
 
     $(window.mediaElement).on("loadstart", function() {
         window.isPreloading = false;
-        hideBufferAnimation();
         if (!$("#video-area").is(":visible")) {
             $("#video-area").fadeIn();
         }
@@ -53,6 +50,14 @@ window.onload = function() {
     $(window.mediaElement).on("abort ended error", function(e) {
         console.log(e);
         $("#video-area").fadeOut(200);
+    });
+
+    $(window.mediaElement).on("playing", function(e) {
+        hideBufferAnimation();
+    });
+
+    $(window.mediaElement).on("waiting", function(e) {
+        showBufferAnimation();
     });
 
     $(window.mediaElement).on("error", function(e) {
@@ -123,46 +128,10 @@ window.onload = function() {
                 $("#video-area").fadeIn(500, showBufferAnimation);
                 window.isPreloading = true;
             } else if (data.action == "buffer.start" && !$("#buffer").is(":visible")) {
-                console.log("buffer");
-                showBufferAnimation();
-
-                // Run an interval to see when the video has finished buffering and starts to play
-                window.lastPlayTime = window.mediaElement.currentTime;
-                if (window.bufferTimer) {
-                    clearInterval(window.bufferTimer);
-                }
-                window.bufferChangeCount = 0;
-                window.bufferTimer = setInterval(function() {
-                    if (window.mediaElement.currentTime > (1 / BUFFER_END_TIMEOUT + window.lastPlayTime) && !window.mediaElement.paused) {
-                        window.bufferChangeCount++;
-                        // If user seeks multiple times, then animation would be removed, we avoid with the count
-                        if (window.bufferChangeCount > 2) {
-                            hideBufferAnimation();
-                        }
-                    } else {
-                        window.bufferChangeCount = 0;
-                    }
-                    window.lastPlayTime = window.mediaElement.currentTime;
-                }, BUFFER_END_TIMEOUT);
             } else if (data.action == "buffer.precentage") {
                 // Never show 100% because it could be waiting a little longer
                 // Also never show a lower number than what is already being shown
                 var percent = parseInt(data.percentage, 10);
-
-                // Apply a hack that would ask the app for more data if chromecast stalls on seek
-                if (percent >= 100 && window.isBuffering && window.isPreloading) {
-                    if (window.seekPokeTimer) {
-                        clearInterval(window.seekPokeTimer);
-                    }
-                    window.seekPokeTimer = setInterval(function() {
-                        try {
-                            window.mediaElement.play().then(window.mediaElement.pause);
-                        } catch (e) {
-                            console.warn(e);
-                        }
-                    }, 1000);
-                }
-
                 if (percent > window.lastBufferPercent) {
                     window.lastBufferPercent = Math.min(percent, 99);
                     $("#progress").text(window.lastBufferPercent + "%");
